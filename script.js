235c9de3cb3b5afce0698c6ec3d5dd39dd0427e6
Charles <GUITARPLRC@YAHOO.COM>
2016-04-05 10:36:02 -0500

    display temp and convert buttons work

--- script.js
+++ script.js
@@ -2,13 +2,8 @@ $(function(){
 	
 	$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Schaumburg,IL&APPID=0e61c9d5e0a1848bbb9d99132298b339",function(result){
 		
-		var temp = result.main.temp;
-		
-    document.write("City: "+ result.name + "<br />");
-    document.write("Weather: "+ result.weather[0].description + "<br />");
-		document.write((temp - 273.15).toFixed(2) + "&degC" + "<br />");
-		document.write(((1.8*(temp - 273))+32).toFixed(2) + "&degF" + "<br />");
-		document.write(temp + "&degK");
+		temp = result.main.temp;
+		convertFah.click;
 		
 		if ((result.weather[0].description).indexOf("rain")) {
 			$("body").css("background-image", "rainy.jpg");
@@ -26,26 +21,26 @@ var convertC = document.getElementById("convertC");
 var convertF = document.getElementById("convertF");
 var symbol = document.getElementById("symbol"); // for weather symbol
 var displayTemp = document.getElementById("temp");
+var city;
+var weather;
 var temp;
 var descript; // to convert from symbol use
 
 convertC.addEventListener("click", convertCel, false);
 convertF.addEventListener("click", convertFah, false);
 
-
-function gettingJSON(){
-	$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London&APPID=0e61c9d5e0a1848bbb9d99132298b339", function(a) {
-		
-		temp = a.main.temp;
-		descript = a.weather[0].description;
-		
-	});
-}
-
 function convertCel() {
-	temp = temp - 273.15;
+	var cTemp; // temporary to hold celsius value
+	convertC.style.visibility = "hidden";
+	convertF.style.visibility = "visible";
+	cTemp = (temp - 273.15).toFixed(2);
+	displayTemp.textContent = cTemp + "&degC";
 }
 
 function convertFah() {
-	temp = 1.8 * (temp - 273) + 32;
+	var fTemp; // temporary to hold fahrenheit value
+	convertF.style.visibility = "hidden";
+	convertC.style.visibility = "visible";
+	fTemp = (1.8 * (temp - 273) + 32).toFixed(2);
+	displayTemp.textContent = fTemp + "&degF";
 }
