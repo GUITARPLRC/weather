651c7da97965a35b215a3b1a070c2278fc20b91b
Charles <GUITARPLRC@YAHOO.COM>
2016-04-05 12:05:39 -0500

    trying to add get location params

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-var convertC = document.getElementById("convertC");
-var convertF = document.getElementById("convertF");
-var symbol = document.getElementById("symbol"); // for weather symbol
-var displayTemp = document.getElementById("temp");
-var message = document.getElementById("message");
-var city;
-var weather;
-var temp;
-var descript; // to convert from symbol use
-
 $(function(){
 	
-	$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Schaumburg,IL&APPID=0e61c9d5e0a1848bbb9d99132298b339",function(result){
+	var convertC = document.getElementById("convertC");
+	var convertF = document.getElementById("convertF");
+	var symbol = document.getElementById("symbol"); // for weather symbol
+	var displayTemp = document.getElementById("temp");
+	var message = document.getElementById("message");
+	var myCity = document.getElementById("myCity");
+	var city;
+	var weather;
+	var temp;
+	var descript; // to convert from symbol use
+	var latitude;
+	var longitude;
+	
+	function storePosition(position) {
+		latitude = (position.coords.latitude).toFixed(2);
+		longitude = (position.coords.longitude).toFixed(2);
+	}
+	
+	navigator.geolocation.getCurrentPosition(storePosition); // get local coordinates
+	
+	console.log(latitude);
+	
+	$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=0e61c9d5e0a1848bbb9d99132298b339",function(result){
 		
 		temp = result.main.temp;
-		message.textContent = result.weather[0].description;
+		city = result.name;
+		descript = result.weather[0].description;
 		
 		if ((result.weather[0].description).indexOf("rain")) {
 			$("body").css("background-image", "url(rainy.jpg)");
@@ -28,25 +41,28 @@ $(function(){
 		
   });
 	
+	convertC.addEventListener("click", convertCel, false);
+	convertF.addEventListener("click", convertFah, false);
+
+	function convertCel() {
+		var cTemp; // temporary to hold celsius value
+		convertC.style.visibility = "hidden";
+		convertF.style.visibility = "visible";
+		cTemp = (temp - 273.15).toFixed(2);
+		displayTemp.textContent = cTemp + "\u00B0C"; //display temp value and add degree
+	}
+
+	function convertFah() {
+		var fTemp; // temporary to hold fahrenheit value
+		convertF.style.visibility = "hidden";
+		convertC.style.visibility = "visible";
+		fTemp = (1.8 * (temp - 273) + 32).toFixed(2);
+		displayTemp.textContent = fTemp + "\u00B0F"; //display temp value and add degree
+	}
+
+	myCity.textContent = city;
+	message.textContent = descript;
+	convertF.click();
+	
+	
 });
-
-convertC.addEventListener("click", convertCel, false);
-convertF.addEventListener("click", convertFah, false);
-
-convertF.click();
-
-function convertCel() {
-	var cTemp; // temporary to hold celsius value
-	convertC.style.visibility = "hidden";
-	convertF.style.visibility = "visible";
-	cTemp = (temp - 273.15).toFixed(2);
-	displayTemp.textContent = cTemp + "\u00B0C"; //display temp value and add degree
-}
-
-function convertFah() {
-	var fTemp; // temporary to hold fahrenheit value
-	convertF.style.visibility = "hidden";
-	convertC.style.visibility = "visible";
-	fTemp = (1.8 * (temp - 273) + 32).toFixed(2);
-	displayTemp.textContent = fTemp + "\u00B0F"; //display temp value and add degree
-}
