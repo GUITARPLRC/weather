04b2d7b2272cd4b0f5411709c5a0300a7ae8f4b1
Charles <GUITARPLRC@YAHOO.COM>
2016-04-07 11:23:14 -0500

    fixed async issue, need fix info display

--- script.js
+++ script.js
@@ -1,37 +1,33 @@
-$(function(){
-	
-	var convertC = $("convertC");
-	var convertF = $("convertF");
-	var symbol = $("symbol"); // for weather symbol
-	var displayTemp = $("temperature");
-	var message = $("message");
-	var myCity = $("myCity");
-	var city; // store city name
-	var tempNow; // store temp value
-	var latitude; // store latitude location
-	var longitude; // store longitude location
-	var descript; // to convert from symbol use
-	var myLocation; // store location info
-	
-	convertC.click(convertCel);
-	convertF.click(convertFah);
-	
-	if ("geolocation" in navigator) {
-		navigator.geolocation.getCurrentPosition(function(position) {
-			console.log(position.coords.latitude);
-			myLat = (position.coords.latitude).toFixed(2);
-			myLong = (position.coords.longitude).toFixed(2);
-			myLocation = "lat="+myLat+"&lon="+myLong;
-		})
-	} else {
-		myLocation = "q=england";
-	}
-	
-	console.log(myLocation);
-	
-	$.getJSON("http://api.openweathermap.org/data/2.5/weather?" + myLocation + "&appid=0e61c9d5e0a1848bbb9d99132298b339", function(info){
+var convertC = $("convertC");
+var convertF = $("convertF");
+var displayTemp = $("temperature");
+var message = $("message");
+var myCity = $("myCity");
+var city; // store city name
+var tempNow; // store temp value
+var descript; // to convert from symbol use
+var myLocation; // store location info
+var myLat;
+var myLong;
+
+convertC.click(convertCel);
+convertF.click(convertFah);
+
+if ("geolocation" in navigator) {
+	navigator.geolocation.getCurrentPosition(function (position) {
+		myLat = (position.coords.latitude).toFixed(2);
+		myLong = (position.coords.longitude).toFixed(2);
+		myLocation = "lat=" + myLat + "&lon=" + myLong;
+		getWeather();
+	});
+} else {
+	myLocation = "q=england";
+	getWeather();
+}
+
+function getWeather() {
+	$.getJSON("http://api.openweathermap.org/data/2.5/weather?" + myLocation + "&appid=0e61c9d5e0a1848bbb9d99132298b339", function (info){
 		
-		console.log(info["coord"]);
 		tempNow = info.main.temp;
 		city = info.name;
 		descript = info.weather[0].description;
@@ -47,27 +43,25 @@ $(function(){
 			
 		}
 		
-  });
+	});
+}
 
-	function convertCel() {
-		var cTemp; // temporary to hold celsius value
-		convertC.style.visibility = "hidden";
-		convertF.style.visibility = "visible";
-		cTemp = (tempNow - 273.15).toFixed(2);
-		displayTemp.textContent = cTemp + "\u00B0C"; //display temp value and add degree
-	}
+function convertCel() {
+	var cTemp; // temporary to hold celsius value
+	convertC.style.visibility = "hidden";
+	convertF.style.visibility = "visible";
+	cTemp = (tempNow - 273.15).toFixed(2);
+	displayTemp.textContent = cTemp + "\u00B0C"; //display temp value and add degree
+}
 
-	function convertFah() {
-		var fTemp; // temporary to hold fahrenheit value
-		convertF.style.visibility = "hidden";
-		convertC.style.visibility = "visible";
-		fTemp = (1.8 * (tempNow - 273) + 32).toFixed(2);
-		displayTemp.textContent = fTemp + "\u00B0F"; //display temp value and add degree
-	}
+function convertFah() {
+	var fTemp; // temporary to hold fahrenheit value
+	convertF.style.visibility = "hidden";
+	convertC.style.visibility = "visible";
+	fTemp = (1.8 * (tempNow - 273) + 32).toFixed(2);
+	displayTemp.textContent = fTemp + "\u00B0F"; //display temp value and add degree
+}
 
-	myCity.textContent = city;
-	message.textContent = descript;
-	convertF.click;
-	
-	
-});
+myCity.textContent = city;
+message.textContent = descript;
+convertF.click;
