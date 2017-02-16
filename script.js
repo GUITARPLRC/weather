c6a2adb5d64735edee3c28b4931f693bb7ea7b94
Charles <GUITARPLRC@YAHOO.COM>
2017-02-16 09:05:06 -0600

    added google maps location api
    used google instead of geolocation because of security

--- script.js
+++ script.js
@@ -17,28 +17,30 @@ container.addEventListener("click", function() {
 	}
 }, false);
 
-if ("geolocation" in navigator) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    myLat = (position.coords.latitude).toFixed(2);
-    myLong = (position.coords.longitude).toFixed(2);
-    myLocation = "lat=" + myLat + "&lon=" + myLong;
-    getWeather();
-  });
+if (window) {
+	$.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCM9hJnkNSXPlbg3XF-HRPTxzXpQUzhJiw", function(data) {
+		myLat = (data.location.lat).toFixed(2);
+		myLong = (data.location.lng).toFixed(2);
+		myLocation = "lat=" + myLat + "&lon=" + myLong;
+	}).done( function() {
+		getWeather();
+	})
 } else {
-  myLocation = "q=london,uk";
-  getWeather();
+	myLocation = "q=london,uk";
+	getWeather();
 }
 
-function getWeather() {
-  $.getJSON("http://api.openweathermap.org/data/2.5/weather?" + myLocation + "&appid=0e61c9d5e0a1848bbb9d99132298b339", function(info) {
 
-    tempNow = info.main.temp;
-    myCity.textContent = info.name;
-    message.textContent = info.weather[0].description;
-    icon.setAttribute("src", "http://openweathermap.org/img/w/"+ info.weather[0].icon +".png")
-		
-    switch (info.weather[0].icon) {
-      
+function getWeather() {
+	$.getJSON("http://api.openweathermap.org/data/2.5/weather?" + myLocation + "&APPID=0e61c9d5e0a1848bbb9d99132298b339", function(info) {
+
+		tempNow = info.main.temp;
+		myCity.textContent = info.name;
+		message.textContent = info.weather[0].description;
+		icon.setAttribute("src", "http://openweathermap.org/img/w/"+ info.weather[0].icon +".png")
+
+		switch (info.weather[0].icon) {
+
       case "02d": // CLOUDY
       case "04d":
       case "03d":
@@ -81,9 +83,9 @@ function getWeather() {
 				break;
       default: // CLEAR SKY
         $("body").css("background-image", "url(sunny.jpg)");
-      
+
     }
-		
+
 		convertFah();
 
   });
@@ -91,14 +93,22 @@ function getWeather() {
 
 function convertCel() {
 	check = 0;
-  var cTemp; // temporary to hold celsius value
-  cTemp = Math.round(tempNow - 273.15);
-  displayTemp.textContent = cTemp + "\u00B0C"; //display temp value and add degree
+	var cTemp; // temporary to hold celsius value
+	cTemp = Math.round(tempNow - 273.15);
+	if (isNaN(cTemp)) {
+		displayTemp.textContent = "Sorry can not see your location";
+	} else {
+		displayTemp.textContent = cTemp + "\u00B0C"; //display temp value and add degree
+	}
 }
 
 function convertFah() {
 	check = 1;
-  var fTemp; // temporary to hold fahrenheit value
-  fTemp = Math.round(1.8 * (tempNow - 273) + 32);
-  displayTemp.textContent = fTemp + "\u00B0F"; //display temp value and add degree
+	var fTemp; // temporary to hold fahrenheit value
+	fTemp = Math.round(1.8 * (tempNow - 273) + 32);
+	if (isNaN(fTemp)) {
+		displayTemp.textContent = "Sorry can not see your location";
+	} else {
+		displayTemp.textContent = fTemp + "\u00B0F"; //display temp value and add degree
+	}
 }
