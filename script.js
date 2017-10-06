b52e2445a94e4f7d80846955372f1c47c3f267eb
guitarplrc <guitarplrc@yahoo.com>
2017-10-06 14:18:21 -0500

    added unsplash images

--- script.js
+++ script.js
@@ -1,94 +1,89 @@
-var container = document.getElementById("container");
-var displayTemp = document.getElementById("temperature");
-var message = document.getElementById("message");
-var myCity = document.getElementById("myCity");
-var icon = document.getElementById("icon");
+var container = document.getElementById('container');
+var displayTemp = document.getElementById('temperature');
+var message = document.getElementById('message');
+var myCity = document.getElementById('myCity');
+var icon = document.getElementById('icon');
 var myLocation; // store location info
 var myLat;
 var myLong;
 var tempNow;
 var check = 0;
 
-container.addEventListener("click", function() {
-	if (check === 0) {
-		convertFah();
-	} else {
-		convertCel();
-	}
-}, false);
+container.addEventListener(
+	'click',
+	function() {
+		if (check === 0) {
+			convertFah();
+		} else {
+			convertCel();
+		}
+	},
+	false
+);
 
 if (window) {
-	$.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCM9hJnkNSXPlbg3XF-HRPTxzXpQUzhJiw", function(data) {
-		myLat = (data.location.lat).toFixed(2);
-		myLong = (data.location.lng).toFixed(2);
-		myLocation = "lat=" + myLat + "&lon=" + myLong;
-	}).done( function() {
+	$.post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCM9hJnkNSXPlbg3XF-HRPTxzXpQUzhJiw', function(
+		data
+	) {
+		myLat = data.location.lat.toFixed(2);
+		myLong = data.location.lng.toFixed(2);
+		myLocation = 'lat=' + myLat + '&lon=' + myLong;
+	}).done(function() {
 		getWeather();
-	})
+	});
 } else {
-	myLocation = "q=london,uk";
+	myLocation = 'q=london,uk';
 	getWeather();
 }
 
-
 function getWeather() {
-	$.getJSON("http://api.openweathermap.org/data/2.5/weather?" + myLocation + "&APPID=0e61c9d5e0a1848bbb9d99132298b339", function(info) {
+	$.getJSON(
+		'https://api.openweathermap.org/data/2.5/weather?' + myLocation + '&APPID=0e61c9d5e0a1848bbb9d99132298b339',
+		function(info) {
+			tempNow = info.main.temp;
+			myCity.textContent = info.name;
+			message.textContent = info.weather[0].description;
+			icon.setAttribute('src', 'https://openweathermap.org/img/w/' + info.weather[0].icon + '.png');
 
-		tempNow = info.main.temp;
-		myCity.textContent = info.name;
-		message.textContent = info.weather[0].description;
-		icon.setAttribute("src", "http://openweathermap.org/img/w/"+ info.weather[0].icon +".png")
+			switch (info.weather[0].icon) {
+				case '02d': // CLOUDY
+				case '04d':
+				case '03d':
+				case '02n':
+				case '03n':
+				case '04n':
+					$('body').css('background-image', 'url("https://unsplash.it/1920/1080?image=1056")');
+					break;
+				case '09d': // RAIN
+				case '10d':
+				case '10n':
+				case '09n':
+					$('body').css('background-image', 'url("https://unsplash.it/1920/1080?image=680")');
+					break;
+				case '11d': // THUNDERSTORM
+				case '11n':
+					$('body').css('background-image', 'url("https://unsplash.it/1920/1080?image=542")');
+					break;
+				case '13d': // SNOW
+				case '13n':
+					$('body').css('background-image', 'url("https://unsplash.it/1920/1080?image=905")');
+					break;
+				case '50d': // MIST / FOG
+				case '50n':
+					$('body').css('background-image', 'url("https://unsplash.it/1920/1080?image=1045")');
+					break;
+				case '01d': // CLEAR SKY
+				case '01n':
+					$('body').css('background-image', 'url("https://unsplash.it/1920/1080?image=986")');
+					break;
+				default:
+					// CLEAR SKY
+					$('body').css('background-image', 'url("https://unsplash.it/1920/1080?image=1026")');
+			}
 
-		switch (info.weather[0].icon) {
-
-      case "02d": // CLOUDY
-      case "04d":
-      case "03d":
-        $("body").css("background-image", "url(cloudy-field.jpg)");
-        break;
-      case "02n":
-      case "03n":
-      case "04n":
-        $("body").css("background-image", "url(cloudy-sunset.jpg)");
-        break;
-      case "09d": // RAIN
-      case "10d":
-        $("body").css("background-image", "url(rainy.jpg)");
-        break;
-      case "10n":
-      case "09n":
-        $("body").css("background-image", "url(rain-glass.jpe)");
-        break;
-      case "11d": // THUNDERSTORM
-				$("body").css("background-image", "url(lightning-storm.jpg)");
-				break;
-      case "11n":
-        $("body").css("background-image", "url(stormy-beach.jpg)");
-        break;
-      case "13d": // SNOW
-      case "13n":
-        $("body").css("background-image", "url(snowy.jpg)");
-        break;
-      case "50d": // MIST / FOG
-				$("body").css("background-image", "url(fog-day.jpeg)");
-				break;
-      case "50n":
-				$("body").css("background-image", "url(fog-night.jpeg)")
-        break;
-			case "01d": // CLEAR SKY
-				$("body").css("background-image", "url(sunny.jpg)");
-				break;
-			case "01n":
-				$("body").css("background-image", "url(star-night.jpg)");
-				break;
-      default: // CLEAR SKY
-        $("body").css("background-image", "url(sunny.jpg)");
-
-    }
-
-		convertFah();
-
-  });
+			convertFah();
+		}
+	);
 }
 
 function convertCel() {
@@ -96,9 +91,9 @@ function convertCel() {
 	var cTemp; // temporary to hold celsius value
 	cTemp = Math.round(tempNow - 273.15);
 	if (isNaN(cTemp)) {
-		displayTemp.textContent = "Sorry can not see your location";
+		displayTemp.textContent = 'Sorry can not see your location';
 	} else {
-		displayTemp.textContent = cTemp + "\u00B0C"; //display temp value and add degree
+		displayTemp.textContent = cTemp + '\u00B0C'; //display temp value and add degree
 	}
 }
 
@@ -107,8 +102,8 @@ function convertFah() {
 	var fTemp; // temporary to hold fahrenheit value
 	fTemp = Math.round(1.8 * (tempNow - 273) + 32);
 	if (isNaN(fTemp)) {
-		displayTemp.textContent = "Sorry can not see your location";
+		displayTemp.textContent = 'Sorry can not see your location';
 	} else {
-		displayTemp.textContent = fTemp + "\u00B0F"; //display temp value and add degree
+		displayTemp.textContent = fTemp + '\u00B0F'; //display temp value and add degree
 	}
 }
